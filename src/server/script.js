e41f69d761edaf810a9bfeb6e221f5347ebfbc74
Aniket Bhosale <abhosale@gmu.edu>
2024-08-06 14:52:36 -0700

    Update script.js

--- src/server/script.js
+++ src/server/script.js
@@ -1,5 +1,5 @@
 import { gs } from '@servicenow/glide'
-
+const a = 3;
 export function showStateUpdate(current, previous) {
     const currentState = current.getValue('state')
     const previousState = previous.getValue('state')
