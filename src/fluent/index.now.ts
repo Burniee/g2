b2b54799c1ccd16b5eabc13f48e46667dad36f19
System Administrator <admin@example.com>
2024-08-08 14:58:15 -0700

    6.1

--- src/fluent/index.now.ts
+++ src/fluent/index.now.ts
@@ -1,7 +1,7 @@
 import '@servicenow/sdk/global'
 import { BusinessRule, ClientScript } from '@servicenow/sdk/core'
 import { showStateUpdate } from '../server/script.js'
-const b = 6;
+const b = 6.1;
 const c = 16;
 
 //creates a client script that pops up 'Table loaded successfully!!' message everytime todo record is loaded
